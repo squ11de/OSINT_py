695b545317717af0a45c7473ff59388696fb0b25
squ11de <98277869+squ11de@users.noreply.github.com>
2023-12-19 01:41:13 -0600

    Update osint.py

--- osint.py
+++ osint.py
@@ -10,12 +10,18 @@
 
 #OTX - https://otx.alienvault.com/api
 
+#AbuseIPDB - https://www.abuseipdb.com/account/api
+
 
 #You also need to run the following command in terminal  
 #[pip3 install requests] this will install python library needed to make GET requests to Vt, Otx and X-Force
+#[pip3 install bs4] this will be used to help parse spur
+
 
 
 import requests
+from bs4 import BeautifulSoup
+
 
 xforce_api_key = "YOUR_X-FROCE_API_KEY_HERE"
 xforce_api_password = "YOUR_X-FORCE_API_PASSWORD_HERE"
@@ -26,66 +32,116 @@ otx_api_key = "YOUR_OTX_API_KEY_HERE"
 
 
 
-def domain():
-    user_input = input("Please enter a URL: ")
 
-    # X-Force API request
+def domain():
+    user_input = input("Please enter a Domain: ")
+
     try:
-        url = 'https://exchange.xforce.ibmcloud.com/api/url/' + f'{user_input}'
+        url = f"https://www.virustotal.com/api/v3/domains/{user_input}"
+        headers = {"x-apikey": virustotal_api_key}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        malicious_count = analysis_stats.get("malicious", 0)
+        non_malicious_count = analysis_stats.get("undetected", 0) + analysis_stats.get("harmless", 0)
+        registrar = data.get("data", {}).get("attributes", {}).get("registrar", "")
+
+        if malicious_count == 0 and non_malicious_count == 0:
+            print("VirusTotal | Unknown")
+        elif registrar:
+            print(f"VirusTotal | {malicious_count}/{malicious_count + non_malicious_count} | {registrar}")
+        else:
+            print(f"VirusTotal | {malicious_count}/{malicious_count + non_malicious_count}")
+    except:
+        print("VirusTotal | Domain does not exist")
+
+
+
+    try:
+        url = f"https://exchange.xforce.ibmcloud.com/api/url/{user_input}"
         headers = {'Accept': 'application/json'}
         auth = (xforce_api_key, xforce_api_password)
         response = requests.get(url, headers=headers, auth=auth)
         data = response.json()
-        risk_score = data['result']['score']
-        formatted_xf_score = f"X-Force | Risk {risk_score}"
-    except KeyError:
-        formatted_xf_score = "X-Force | Site does not exist"
+        risk_score = data.get('result', {}).get('score')
+        if risk_score is not None:
+            print(f"X-Force | Risk {risk_score}")
+        else:
+            print("X-Force | Risk Unknown")
+    except:
+        print("X-Force | Risk Unknown")
 
-    # VirusTotal API request
     try:
-        virustotal_url = 'https://www.virustotal.com/api/v3/domains/' + f'{user_input}'
-        virustotal_headers = {"x-apikey": virustotal_api_key}
-        virustotal_response = requests.get(virustotal_url, headers=virustotal_headers)
-        virustotal_data = virustotal_response.json()
-        malicious_count = virustotal_data["data"]["attributes"]["last_analysis_stats"]["malicious"]
-        formatted_vt_score = f"VirusTotal | {malicious_count}"
-    except KeyError:
-        formatted_vt_score = "VirusTotal | Site does not exist"
-
-    # OTX API request
+        url = f"https://otx.alienvault.com/api/v1/indicators/domain/{user_input}/"
+        headers = {"X-OTX-API-KEY": otx_api_key, "Accept": "application/json", "User-Agent": "OTX Python SDK"}
+        response = requests.get(url, headers=headers).json()
+        if isinstance(response, dict) and response.get('error') == 'unable to parse domain or hostname':
+            print("OTX | Unknown")
+        else:
+            score = response.get("pulse_info", {}).get("count", 0)
+            print(f"OTX | Pulses {score}")
+    except:
+        print("OTX | Unknown")
     try:
-        otx_url = f"https://otx.alienvault.com/api/v1/indicators/domain/{user_input}/"
-        otx_headers = {"X-OTX-API-KEY": otx_api_key, "Accept": "application/json", "User-Agent": "OTX Python SDK"}
-        otx_response = requests.get(otx_url, headers=otx_headers).json()
-        score = otx_response.get("pulse_info", {}).get("count", 0)
-        formatted_otx_score = f"OTX | pulses {score}"
-    except KeyError:
-        formatted_otx_score = "OTX will not have a key error lol"           
-
-    
-    if formatted_vt_score == "VirusTotal | Site does not exist" and formatted_xf_score == "X-Force | Site does not exist":
-        print("-------------------")  
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print("OTX | Site does not exist")
+        url = f"https://www.whois.com/whois/{user_input}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for pre in soup.find_all('pre'):
+            if "Creation Date:" in pre.text:
+                for line in pre.text.split('\n'):
+                    if line.startswith('Creation Date:'):
+                        creation_date = line.split(': ')[1].strip()
+                        print(f"Whois | Domain {user_input} was created on {creation_date}")
+    except:
+        print("Whois | Domain does not exist")
 
 
-    else:
-        print("-------------------")        
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print(formatted_otx_score)
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def hash():
     user_input = input("Please enter a hash: ")
 
-    # X-Force API request
+   
+    
+    try:
+        url = f"https://www.virustotal.com/api/v3/files/{user_input}"
+        headers = {"x-apikey": virustotal_api_key}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        malicious_count = analysis_stats.get("malicious", 0)
+        total_detections = sum(analysis_stats.values())
+
+        signature_info = data.get("data", {}).get("attributes", {}).get("signature_info", {})
+        signature_verified = signature_info.get("verified", "No verification info found")
+        signature_product = signature_info.get("product", "No product info found")
+
+        if total_detections == 0:
+            print("VirusTotal | Unknown")
+        elif signature_verified != "No verification info found":
+            print("OSINT")
+            print(f"-{user_input}")
+            print(f"VirusTotal | {malicious_count}/{total_detections} | File is signed | {signature_product}")
+        else:
+            print(f"VirusTotal | {malicious_count}/{total_detections} | File is not signed")
+    except:
+        print("Error occurred while getting data from VirusTotal")
+    
     try:
         xforce_url = 'https://exchange.xforce.ibmcloud.com/api/malware/' + f'{user_input}'
         xforce_headers = {'Accept': 'application/json'}
@@ -93,49 +149,37 @@ def hash():
         xforce_response = requests.get(xforce_url, headers=xforce_headers, auth=xforce_auth)
         xforce_data = xforce_response.json()
         xforce_risk_score = xforce_data['malware']['risk']
-        formatted_xforce_risk_score = xforce_risk_score.capitalize()
-        formatted_xf_score = f"X-Force | {formatted_xforce_risk_score}"
+        print(f"X-Force | Risk {xforce_risk_score}")
     except KeyError:
-        formatted_xf_score = "X-Force | Hash does not exist"
+        print("X-Force | Risk Unknown")
 
-    # VirusTotal API request
-    try:
-        virustotal_url = "https://www.virustotal.com/api/v3/files/" + f'{user_input}'
-        virustotal_headers = {"x-apikey": virustotal_api_key}
-        virustotal_response = requests.get(virustotal_url, headers=virustotal_headers)
-        virustotal_data = virustotal_response.json()
-        file_hash = virustotal_data["data"]["id"]
-        result = virustotal_data["data"]["attributes"]["last_analysis_stats"]["malicious"]
-        formatted_vt_score = f"VirusTotal | {result}"
-    except KeyError:
-        formatted_vt_score = "VirusTotal | Hash does not exist"
 
-    # OTX API request
+
+    
     try:
         otx_url = f"https://otx.alienvault.com/api/v1/indicators/file/{user_input}/"
         otx_headers = {"X-OTX-API-KEY": otx_api_key, "Accept": "application/json", "User-Agent": "OTX Python SDK"}
         otx_response = requests.get(otx_url, headers=otx_headers).json()
-        score = otx_response.get("pulse_info", {}).get("count", 0)
-        formatted_otx_score = f"OTX | {score}"
+
+        if isinstance(otx_response, dict) and otx_response.get('detail') == 'endpoint not found':
+            print("OTX | Unknown")
+        else:
+            score = otx_response.get("pulse_info", {}).get("count", 0)
+            formatted_otx_score = f"OTX | {score}"
+            print(formatted_otx_score)
+    
+    
     except KeyError:
-        formatted_otx_score = "OTX | Hash does not exist"
-
-    if formatted_vt_score == "VirusTotal | Hash does not exist" and formatted_xf_score == "X-Force | Hash does not exist":
-        print("-------------------")
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print("OTX | Hash does not exist")
+            print("OTX will not error")
 
 
-    else:
-        print("-------------------")    
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print(formatted_otx_score)
+
+
+
+
+
+
+
 
 
 
@@ -144,55 +188,115 @@ def hash():
 def ip():
     user_input = input("Please enter an IP address: ")
 
+
     try:
-        # X-Force API request
+        url = f"https://www.virustotal.com/api/v3/ip_addresses/{user_input}"
+        headers = {"x-apikey": virustotal_api_key}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        malicious_count = analysis_stats.get("malicious", 0)
+        total_detections = sum(analysis_stats.values())
+
+        country = data.get("data", {}).get("attributes", {}).get("country", "No country info found")
+        if total_detections == 0:
+            print("VirusTotal | Unknown")
+        else:
+            print(f"VirusTotal | {malicious_count}/{total_detections} | {country}")
+    except:
+        print("VirusTotal | Unknown")
+
+    try:
+        abuseipdb_url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={user_input}&maxAgeInDays=90"
+        abuseipdb_headers = {"Accept": "application/json", "Key": abuseipdb_api_key}
+        abuseipdb_response = requests.get(abuseipdb_url, headers=abuseipdb_headers).json()
+        abuseipdb_score = abuseipdb_response["data"]["abuseConfidenceScore"]
+        abuseipdb_country = abuseipdb_response["data"]["countryCode"]
+        abuseipdb_total_reports = abuseipdb_response["data"]["totalReports"]
+        abuseipdb_isp = abuseipdb_response["data"]["isp"]
+        abuseipdb_format_score = (f"AbuseIPDB | IP Score: {abuseipdb_score} | Country: {abuseipdb_country} | Total Reports: {abuseipdb_total_reports} | ISP: {abuseipdb_isp}")
+        print(abuseipdb_format_score)
+   
+    except KeyError:
+        abuseipdb_format_score = "AbuseIPDB | Unknown"
+        print(abuseipdb_format_score)
+
+
+
+    try:
         url = 'https://exchange.xforce.ibmcloud.com/api/ipr/' + f'{user_input}'
         headers = {'Accept': 'application/json'}
-        auth = (xforce_api_key, xforce_api_password)
+        auth = ('ba1a8671-d2c2-4ffd-ba4e-69624afe8c10', '0184d324-d1a1-4687-987c-c51d978c8214')
         response = requests.get(url, headers=headers, auth=auth)
         data = response.json()
         risk_score = data['score']
-        formatted_xf_score = f"X-Force | {risk_score}"
+        print(f"X-Force | Risk {risk_score}")
     except KeyError:    
-        formatted_xf_score = "X-Force | IP does not exist"
+        print("X-Force | Unknown")
+
 
     try:
-        # VirusTotal API request
-        virustotal_url = "https://www.virustotal.com/api/v3/ip_addresses/" + f'{user_input}'
-        virustotal_headers = {"x-apikey": virustotal_api_key}
-        virustotal_response = requests.get(virustotal_url, headers=virustotal_headers)
-        virustotal_data = virustotal_response.json()
-        result = virustotal_data["data"]["attributes"]["last_analysis_stats"]["malicious"]
-        formatted_vt_score = f"VirusTotal | {result}"
-    except KeyError:
-        formatted_vt_score = "VirusTotal | IP does not exist"
-
-    try:
-        # OTX API request
         otx_url = f"https://otx.alienvault.com/api/v1/indicators/IPv4/{user_input}/"
         otx_headers = {"X-OTX-API-KEY": otx_api_key, "Accept": "application/json", "User-Agent": "OTX Python SDK"}
         otx_response = requests.get(otx_url, headers=otx_headers).json()
-        pulse_count = otx_response["pulse_info"]["count"]
-        formatted_otx_score = f"OTX | {pulse_count}"
+        if isinstance(otx_response, dict) and otx_response.get('detail') == f'Invalid IP ({user_input})':
+            print("OTX | Unknown")
+        else:
+            score = otx_response.get("pulse_info", {}).get("count", 0)
+            formatted_otx_score = f"OTX | Pulses {score}"
+            print(formatted_otx_score)
+
     except KeyError:
-        formatted_otx_score = "OTX | IP does not exist"
+        formatted_otx_score = "OTX | Unknown"
+
+
+    try:
+        spur_url = f"https://spur.us/context/{user_input}/"
+        spur_headers = {"Accept": "application/html", }
+
+        response = requests.get(spur_url, headers=spur_headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        description_tag = soup.find('meta', attrs={'name': 'description'})
+        title_tag = soup.find('title')
+
+        if description_tag:
+            description_content = description_tag.get('content').lower()
+            if 'vpn' in description_content and ('is part of' in description_content or 'users can hide their activity' in description_content):
+                title_content = title_tag.get_text()
+                vpn_name = title_content.split('(')[1].split(')')[0]
+                spur_score = f"Spur | This IP is associated with{vpn_name}"
+                print(spur_score)
+            
+            elif "tor" in description_content and ('is part of' in description_content or 'users can hide their activity' in description_content):
+                print("Spur | This IP is associated with Tor.")
+            
+            else:
+                print("Spur | This IP is not associated with a VPN.")
+        else:
+            print("Spur | Unknown")
+    except:
+        print("Error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    
-    if formatted_vt_score == "VirusTotal | IP does not exist" and formatted_xf_score == "X-Force | IP does not exist":
-        print("-------------------")
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print("OTX | IP does not exist")
-        
-    else:
-        print("-------------------")   
-        print("OSINT")
-        print("-" + user_input)
-        print(formatted_vt_score)
-        print(formatted_xf_score)
-        print(formatted_otx_score)
 
 if __name__ == "__main__":
     
@@ -219,7 +323,7 @@ if __name__ == "__main__":
     print(ascii_art)
     while True:
         print()
-        ur_choice = input("Would you like to perform OSINT on a Hash, Domain, or IP address? ").lower()
+        ur_choice = input("Would you like to perform OSINT on a Hash, Domain, or IP? ").lower()
         if ur_choice == "hash":
             hash()
             break
@@ -230,6 +334,6 @@ if __name__ == "__main__":
             ip()
             break
         else:
-            print("Invalid input. Please enter Hash, Domain, or IP. Check your spelling!")
+            print("Invalid input. Please enter hash, domain, or ip. Check your spelling!")
 
 
